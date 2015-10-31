9f317c2f554b94c049abd6215c5ff75ec79c46c3
Steffen <dudedbz@gmail.com>
2015-10-31 21:09:35 +0100

    DeffieHellman now uses modPow (This is faster)

--- API/src/div/Encryption.java
+++ API/src/div/Encryption.java
@@ -5,6 +5,57 @@ import java.math.BigInteger;
 public class Encryption
 {
     /**
+     * check whether p is a safe 2048-bit prime (meaning that both p and (p-1)/2 are prime and that 2^2047 < p < 2^2048)
+     * 
+     * @param p
+     *            the prime
+     * @return true if its safe
+     */
+    public static boolean validatePrime(BigInteger p)
+    {
+        BigInteger TWO = BigInteger.ONE.add(BigInteger.ONE);
+
+        if (p.isProbablePrime(1))
+        {
+            if (p.subtract(BigInteger.ONE).divide(TWO).isProbablePrime(1))
+            {
+                if (TWO.pow(2047).compareTo(p) == -1)
+                {
+                    if (TWO.pow(2048).compareTo(p) == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Make sure that g, g_a, and g_b > 1 && less than p-1
+     * 
+     * @param g
+     *            the base
+     * @param g_a
+     *            person As' public key
+     * @param g_b
+     *            person Bs' public key
+     * @return true if valid
+     */
+    public static boolean validatePublic(BigInteger p, BigInteger g, BigInteger g_a, BigInteger g_b)
+    {
+        if (g.compareTo(BigInteger.ONE) == 1 && g_a.compareTo(BigInteger.ONE) == 1 && g_b.compareTo(BigInteger.ONE) == 1)
+        {
+            BigInteger p_1 = p.subtract(BigInteger.ONE);
+            if (g.compareTo(p_1) == -1 && g_a.compareTo(p_1) == -1 && g_b.compareTo(p_1) == -1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * Get the secret shared key from a public key
      * 
      * @param prime
@@ -17,19 +68,18 @@ public class Encryption
      */
     public static BigInteger diffieHellmanSecretKey(BigInteger prime, BigInteger publicKey, BigInteger secretKey)
     {
-        BigInteger publicPowSecret = publicKey.pow(secretKey.intValueExact());
-        BigInteger remainder = publicPowSecret.remainder(prime);
+        BigInteger sharedSecret = publicKey.modPow(secretKey, prime);
 
-        return remainder;
+        return sharedSecret;
     }
 
     /**
      * Generate a public key that you can use to encrypt data in a two-way conversation
      * 
      * @param prime
-     *            a prime number (larger is better) BigInteger.probablePrime(4096, new Random()) is preferred;
+     *            a prime number (larger is better)
      * @param primitiveRoot
-     *            (doesn't really matter, 2,3, etc is fine) 
+     *            (doesn't really matter, 2,3, etc is fine)
      * @param privateKey
      *            your private key
      * 
@@ -37,11 +87,8 @@ public class Encryption
      */
     public static BigInteger diffieHellmanPublicKey(BigInteger prime, BigInteger primitiveRoot, BigInteger privateKey)
     {
-        BigInteger gPowX = primitiveRoot.pow(privateKey.intValueExact());
-        BigInteger gPowXModP = gPowX.remainder(prime);
-
-        return gPowXModP;
+        BigInteger publicKey = primitiveRoot.modPow(privateKey, prime);
+        return publicKey;
     }
-    
-    
+
 }
