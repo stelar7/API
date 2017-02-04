7a4c118b1ab6166ed0bf24e4e7660a0036552e87
stelar7 <dudedbz@gmail.com>
2017-02-04 12:25:45 +0100

    Fix code that has been broken for ages...........

--- API/src/main/java/div/Waybill.java
+++ API/src/main/java/div/Waybill.java
@@ -1,242 +1,152 @@
 package div;
 
-import com.google.gson.*;
-import com.google.gson.reflect.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.*;
 
 public class Waybill
 {
-    
-    
-    publ puble
-    finaeciver;
-    private Location pickup;
-    private Location dropoff;
-    
-    private String transporter;
-    
-    private String   productType;
-    private String   notes;
-    private BaseData data;
-    
-    private List<Product> goods = new ArrayList<>();
-    
-    public void addProduct(final Product p)
+    public static class BaseData
     {
-        this.goods.add(p);
-    }
-    
-    public BaseData getData()
-    {
-        return this.data;
-    }
-    
-    public Location getDropoff()
-    {
-        return this.dropoff;
-    }
-    
-    public List<Product> getGoods()
-    {
-        return this.goods;
-    }
-    
-    public String getNotes()
-    {
-        return this.notes;
-    }
-    
-    public Location getPickup()
-    {
-        return this.pickup;
-    }
-    
-    public String getProductType()
-    {
-        return this.productType;
-    }
-    
-    public Location getReciver()
-    {
-        return this.reciver;
-    }
-    
-    public Location getSender()
-    {
-        return this.sender;
-    }
-    
-    public String getTransporter()
-    {
-        return this.transporter;
-    }
-    
-    public void setData(final BaseData data)
-    {
-        this.data = data;
-    }
-    
-    public void setDropoff(final Location dropoff)
-    {
-        public enum Incoterm
+        private final LocationReference sender;
+        private final LocationReference reciver;
+        private final Incoterm          type;
+        
+        public BaseData(final LocationReference sender, final LocationReference reciver, final Incoterm type)
         {
-            EXW,
-            FCA,
-            CPT,
-            CIP,
-            DAT,
-            DAP,
-            DDP,
-            FAS,
-            FOB,
-            CFR,
-            CIF,
-            DAF,
-            DES,
-            DEQ,
-            DDU
+            super();
+            this.sender = sender;
+            this.reciver = reciver;
+            this.type = type;
         }
-        this.dropoff = dropoff;
-    }
-    
-    public void setGoods(final List<Product> goods)
-    {
-        public static class BaseData
+        
+        public LocationReference getReciver()
         {
-            private final LocationReference sender;
-            private final LocationReference reciver;
-            private final Incoterm          type;
-        
-            public BaseData(final LocationReference sender, final LocationReference reciver, final Incoterm type)
-            {
-                super();
-                this.sender = sender;
-                this.reciver = reciver;
-                this.type = type;
-            }
-        
-            public LocationReference getReciver()
-            {
-                return this.reciver;
-            }
-        
-            public LocationReference getSender()
-            {
-                return this.sender;
-            }
-        
-            public Incoterm getType()
-            {
-                return this.type;
-            }
-        
-            @Override
-            public String toString()
-            {
-                return "BaseData [sender=" + this.sender + ", reciver=" + this.reciver + ", type=" + this.type + "]";
-            }
+            return this.reciver;
         }
-        this.goods = new ArrayList<>(goods);
-    }
-    
-    public void setNotes(final String notes)
-    {
-    
-        public static class Hazard
+        
+        public LocationReference getSender()
         {
-            private final UnNumber unNumber;
-            private final double   amount;
-            private final String   amountSpec; // kg/ltr
-        
-            public Hazard(final int id, final double amount, final String amountSpec)
-            {
-                super();
-                this.unNumber = UnNumber.from(id);
-                this.amount = amount;
-                this.amountSpec = amountSpec;
-            }
-        
-            public double getAmount()
-            {
-                return this.amount;
-            }
-        
-            public String getAmountSpec()
-            {
-                return this.amountSpec;
-            }
-        
-            public String getDescription()
-            {
-                return this.unNumber.desc;
-            }
-        
-            public String getGroup()
-            {
-                return this.unNumber.clazz;
-            }
-        
-            public int getId()
-            {
-                return this.unNumber.number;
-            }
-        
-            @Override
-            public String toString()
-            {
-                return "Hazard [" + this.unNumber + ", amount=" + this.amount + ", amountSpec=" + this.amountSpec + "]";
-            }
-        
+            return this.sender;
         }
-        this.notes = notes;
-    }
-    
-    public void setPickup(final Location pickup)
-    {
-    
-        public static class Location
+        
+        public Incoterm getType()
         {
-            private final String name;
-            private final String address;
-        
-            private final PostCode area;
-        
-            public Location(final String name, final String address, final PostCode area)
-            {
-                super();
-                this.name = name;
-                this.address = address;
-                this.area = area;
-            }
-        
-            public String getAddress()
-            {
-                return this.address;
-            }
-        
-            public PostCode getArea()
-            {
-                return this.area;
-            }
-        
-            public String getName()
-            {
-                return this.name;
-            }
-        
-            @Override
-            public String toString()
-            {
-                return "Location [name=" + this.name + ", address=" + this.address + ", area=" + this.area + "]";
+            return this.type;
         }
+        
+        @Override
+        public String toString()
+        {
+            return "BaseData [sender=" + this.sender + ", reciver=" + this.reciver + ", type=" + this.type + "]";
         }
-        this.pickup = pickup;
     }
     
-    public void setProductType(final String productType)
-
+    public static class Hazard
+    {
+        private final UnNumber unNumber;
+        private final double   amount;
+        private final String   amountSpec; // kg/ltr
+        
+        public Hazard(final int id, final double amount, final String amountSpec)
+        {
+            super();
+            this.unNumber = UnNumber.from(id);
+            this.amount = amount;
+            this.amountSpec = amountSpec;
+        }
+        
+        public double getAmount()
+        {
+            return this.amount;
+        }
+        
+        public String getAmountSpec()
+        {
+            return this.amountSpec;
+        }
+        
+        public String getDescription()
+        {
+            return this.unNumber.desc;
+        }
+        
+        public String getGroup()
+        {
+            return this.unNumber.clazz;
+        }
+        
+        public int getId()
+        {
+            return this.unNumber.number;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "Hazard [" + this.unNumber + ", amount=" + this.amount + ", amountSpec=" + this.amountSpec + "]";
+        }
+        
+    }
+    
+    public enum Incoterm
+    {
+        EXW,
+        FCA,
+        CPT,
+        CIP,
+        DAT,
+        DAP,
+        DDP,
+        FAS,
+        FOB,
+        CFR,
+        CIF,
+        DAF,
+        DES,
+        DEQ,
+        DDU
+    }
+    
+    public static class Location
+    {
+        private final String name;
+        private final String address;
+        
+        private final PostCode area;
+        
+        public Location(final String name, final String address, final PostCode area)
+        {
+            super();
+            this.name = name;
+            this.address = address;
+            this.area = area;
+        }
+        
+        public String getAddress()
+        {
+            return this.address;
+        }
+        
+        public PostCode getArea()
+        {
+            return this.area;
+        }
+        
+        public String getName()
+        {
+            return this.name;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "Location [name=" + this.name + ", address=" + this.address + ", area=" + this.area + "]";
+        }
+    }
+    
     public static class LocationReference
     {
         private final Long transporterId;
@@ -256,55 +166,43 @@ public class Waybill
         }
     }
     
+    public static class PostCode
     {
-        this.productType = productType;
-    }
-    
-    public void setReciver(final Location reciver)
-    {
-    
-        public static class PostCode
+        private Long code;
+        
+        private final String name;
+        
+        public PostCode(final Long code, final String name)
         {
-            private final String name;
-            private       Long   code;
-        
-            public PostCode(final Long code, final String name)
-            {
-                super();
-                this.code = code;
-                this.name = name;
-            }
-        
-            public PostCode(final String other)
-            {
-                super();
-                this.name = other;
-            }
-        
-            public Long getCode()
-            {
-                return this.code;
-            }
-        
-            public String getName()
-            {
-                return this.name;
-            }
-        
-            @Override
-            public String toString()
-            {
-                return "PostCode [code=" + this.code + ", name=" + this.name + "]";
-            }
+            super();
+            this.code = code;
+            this.name = name;
         }
-    
-        this.reciver = reciver;
+        
+        public PostCode(final String other)
+        {
+            super();
+            this.name = other;
+        }
+        
+        public Long getCode()
+        {
+            return this.code;
+        }
+        
+        public String getName()
+        {
+            return this.name;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "PostCode [code=" + this.code + ", name=" + this.name + "]";
+        }
     }
     
-    public void setSender(final Location sender)
-    {
-    
-        ic static class Product
+    public static class Product
     {
         private final String mark;
         private final String name;
@@ -386,28 +284,9 @@ public class Waybill
         
     }
     
-        this.sender = sender;
-    }
-    
-    public void setTransporter(final String transporter)
-    
-    ic
-    
-    static class UnNumber
+    public static class UnNumber
     {
         private static volatile Map<Integer, UnNumber> numbers;
-        private final           int                    number;
-        privatefinal String
-        clazz;
-        final String desc;
-        
-        public UnNumber(final int id, final String clazz, final String desc)
-        {
-            super();
-            this.number = id;
-            this.clazz = clazz;
-            this.desc = desc;
-        }
         
         public static UnNumber from(final int id)
         {
@@ -432,6 +311,20 @@ public class Waybill
             return UnNumber.numbers.get(id);
         }
         
+        private final int number;
+        
+        private final String clazz;
+        
+        private final String desc;
+        
+        public UnNumber(final int id, final String clazz, final String desc)
+        {
+            super();
+            this.number = id;
+            this.clazz = clazz;
+            this.desc = desc;
+        }
+        
         @Override
         public String toString()
         {
@@ -441,28 +334,18 @@ public class Waybill
     
     public static class Volume
     {
-        privat
-    
+        private final Long width;
+        private final Long height;
+        private final Long depth;
+        
+        // IN CM!!
+        public Volume(final Long width, final Long depth, final Long height)
         {
-            this.transporter = transporter;
+            super();
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
         }
-    
-        @Override
-        public String toString()
-        {
-        
-            l Long width;
-            private final Long height;
-            private final Long depth;
-        
-            // IN CM!!
-        public Volume( final Long width, final Long depth, final Long height)
-            {
-                super();
-                this.width = width;
-                this.height = height;
-                this.depth = depth;
-            }
         
         public Long getDepth()
         {
@@ -476,7 +359,7 @@ public class Waybill
         
         public double getLM()
         {
-            return (((double) this.width / 100D) * (((double) this.depth) / 100D)) / 2.4D;
+            return ((double) this.width / 100D * (((double) this.depth) / 100D)) / 2.4D;
         }
         
         public double getVolume()
@@ -497,22 +380,132 @@ public class Waybill
     }
     
     private Location sender;
-    private Location rfinal
-    StringJoiner returnJoiner = new StringJoiner("\n\t", "Waybill [\n\t", "]");
-    final StringJoiner productJoiner = new StringJoiner("\n\t\t", "[\n\t\t", "\n\t\t]\n\t");
-        this.goods.forEach(a ->productJoiner.add(a.toString()));
+    private Location reciver;
+    private Location pickup;
+    private Location dropoff;
+    
+    private String transporter;
+    
+    private String   productType;
+    private String   notes;
+    private BaseData data;
+    
+    private List<Product> goods = new ArrayList<>();
+    
+    public void addProduct(final Product p)
+    {
+        this.goods.add(p);
+    }
+    
+    public BaseData getData()
+    {
+        return this.data;
+    }
+    
+    public Location getDropoff()
+    {
+        return this.dropoff;
+    }
+    
+    public List<Product> getGoods()
+    {
+        return this.goods;
+    }
+    
+    public String getNotes()
+    {
+        return this.notes;
+    }
+    
+    public Location getPickup()
+    {
+        return this.pickup;
+    }
+    
+    public String getProductType()
+    {
+        return this.productType;
+    }
+    
+    public Location getReciver()
+    {
+        return this.reciver;
+    }
+    
+    public Location getSender()
+    {
+        return this.sender;
+    }
+    
+    public String getTransporter()
+    {
+        return this.transporter;
+    }
+    
+    public void setData(final BaseData data)
+    {
+        this.data = data;
+    }
+    
+    public void setDropoff(final Location dropoff)
+    {
+        this.dropoff = dropoff;
+    }
+    
+    public void setGoods(final List<Product> goods)
+    {
+        this.goods.clear();
+        this.goods.addAll(goods);
+    }
+    
+    public void setNotes(final String notes)
+    {
+        this.notes = notes;
+    }
+    
+    public void setPickup(final Location pickup)
+    {
+        this.pickup = pickup;
+    }
+    
+    public void setProductType(final String productType)
+    {
+        this.productType = productType;
+    }
+    
+    public void setReciver(final Location reciver)
+    {
+        this.reciver = reciver;
+    }
+    
+    public void setSender(final Location sender)
+    {
+        this.sender = sender;
+    }
+    
+    public void setTransporter(final String transporter)
+    {
+        this.transporter = transporter;
+    }
+    
+    @Override
+    public String toString()
+    {
+        final StringJoiner returnJoiner  = new StringJoiner("\n\t", "Waybill [\n\t", "]");
+        final StringJoiner productJoiner = new StringJoiner("\n\t\t", "[\n\t\t", "\n\t\t]\n\t");
+        this.goods.forEach(a -> productJoiner.add(a.toString()));
         
-        returnJoiner.add("Sender: "+this.sender);
-        returnJoiner.add("Pickup: "+this.pickup);
-        returnJoiner.add("Reciver: "+this.reciver);
-        returnJoiner.add("Dropoff: "+this.dropoff);
-        returnJoiner.add("Transporter: "+this.transporter);
-        returnJoiner.add("ProductType: "+this.productType);
-        returnJoiner.add("Notes: \n\n"+this.notes);
-        returnJoiner.add("\n\tData: "+this.data);
-        returnJoiner.add("Goods: \t"+productJoiner);
+        returnJoiner.add("Sender: " + this.sender);
+        returnJoiner.add("Pickup: " + this.pickup);
+        returnJoiner.add("Reciver: " + this.reciver);
+        returnJoiner.add("Dropoff: " + this.dropoff);
+        returnJoiner.add("Transporter: " + this.transporter);
+        returnJoiner.add("ProductType: " + this.productType);
+        returnJoiner.add("Notes: \n\n" + this.notes);
+        returnJoiner.add("\n\tData: " + this.data);
+        returnJoiner.add("Goods: \t" + productJoiner);
         
         return returnJoiner.toString();
-}
+    }
     
 }
